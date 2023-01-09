cd7332ae366a491f277ddba8b85d46a2b0c13b03
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-09 17:38:55 -0300

    Adiciona programa que calcula a área de dois triângulos, comparando suas áreas. Usa Orientação a Objetos

---
+++ CalcularAreaComOO/CalcularAreaComOO/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CalcularAreaComOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float aX = float.Parse(Console.ReadLine());
+            float bX = float.Parse(Console.ReadLine());
+            float cX = float.Parse(Console.ReadLine());
+            Triangulo x = new Triangulo(aX, bX, cX);
+
+            float aY = float.Parse(Console.ReadLine());
+            float bY = float.Parse(Console.ReadLine());
+            float cY = float.Parse(Console.ReadLine());
+            Triangulo y = new Triangulo(aY, bY, cY);
+
+            double areaX = x.CalcularArea();
+            double areaY = y.CalcularArea();
+
+            Console.WriteLine($"Área de X = {areaX.ToString("F4")}");
+            Console.WriteLine($"Área de Y = {areaY.ToString("F4")}");
+
+            if(areaX > areaY)
+            {
+                Console.WriteLine("Maior área = X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área = Y");
+            }
+            
+        }
+    }
+}
---
+++ CalcularAreaComOO/CalcularAreaComOO/Triangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcularAreaComOO
+{
+    public class Triangulo
+    {
+        public Triangulo(float a, float b, float c)
+        {
+            this.a = a; 
+            this.b = b;
+            this.c = c;
+        }
+
+        float a;
+        float b;
+        float c;
+
+        public double CalcularArea()
+        {
+            double p = (a + b + c) / 2;
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return area;
+        }
+    }
+}
