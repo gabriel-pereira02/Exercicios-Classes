32aeb91af7fd13029a027517f3b4658e205c0306
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-09 18:34:33 -0300

    Adiciona Ex02

---
+++ Ex02/Ex02/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario funcionarioA = new Funcionario(); 
+            Funcionario funcionarioB = new Funcionario();
+
+            Console.WriteLine("Dados do primeiro funcionário: ");
+            Console.Write("Nome: ");
+            funcionarioA.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionarioA.salario = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do segundo funcionário: ");
+            Console.Write("Nome: ");
+            funcionarioB.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            funcionarioB.salario = float.Parse(Console.ReadLine());
+
+            float salarioMedio = (funcionarioA.salario + funcionarioB.salario) / 2;
+            Console.WriteLine($"Salário médio: {salarioMedio}");
+
+        }
+    }
+}
