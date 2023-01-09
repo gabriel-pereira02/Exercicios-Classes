e6ce458517d3d1fa8cdb0a1df3b0df201309728b
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-09 18:06:26 -0300

    Adiciona Ex01

---
+++ Ex01/Ex01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Pessoa pessoaA = new Pessoa();
+            Console.Write("Nome: ");
+            pessoaA.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoaA.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Pessoa pessoaB = new Pessoa();
+            Console.Write("Nome: ");
+            pessoaB.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoaB.idade = int.Parse(Console.ReadLine());
+
+            if(pessoaA.idade > pessoaB.idade)
+            {
+                Console.WriteLine($"Pessoa mais velha: {pessoaA.nome}");
+            }
+            else
+            {
+                Console.WriteLine($"Pessoa mais velha: {pessoaB.nome}");
+            }
+
+
+        }
+    }
+}
