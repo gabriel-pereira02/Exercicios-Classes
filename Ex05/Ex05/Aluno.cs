94ef46020316a6772a42848a3304dd908d81aba8
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-11 14:36:23 -0300

    Adiciona Ex05

---
+++ Ex05/Ex05/Aluno.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05
+{
+    public class Aluno
+    {
+        public string Nome;
+        public float NotaA;
+        public float NotaB;
+        public float NotaC;
+
+        public float NotaFinal()
+        {
+            return NotaA + NotaB + NotaC;
+        }
+
+        public bool Aprovado()
+        {
+            if (NotaFinal() >= 60)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public float Restante()
+        {
+            if (Aprovado())
+            {
+                return 0;
+            }
+            else
+            {
+                return 60 - NotaFinal();
+            }
+        }
+    }
+}
---
+++ Ex05/Ex05/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Ex05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno01 = new Aluno();
+            Console.Write("Nome do Aluno: ");
+            aluno01.Nome = Console.ReadLine();
+            Console.WriteLine("Insira suas três notas: ");
+            aluno01.NotaA = float.Parse(Console.ReadLine());
+            aluno01.NotaB = float.Parse(Console.ReadLine());
+            aluno01.NotaC = float.Parse(Console.ReadLine());
+
+            float notaFinal = aluno01.NotaFinal();
+
+            if (aluno01.Aprovado().Equals(true))
+            {
+                Console.WriteLine($"NOTA FINAL - {notaFinal}");
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine($"NOTA FINAL - {notaFinal}");
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine($"PONTOS RESTANTES - {aluno01.Restante()}");
+            }
+        }
+    }
+}
