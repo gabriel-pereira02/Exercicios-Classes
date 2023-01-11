fbb9007820e3db752dbeddee069da832e446742e
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-11 14:13:53 -0300

    Adiciona Ex04

---
+++ Ex04/Ex04/Funcionarios.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex04
+{
+    public class Funcionarios
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiqudo()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto * ((porcentagem/100) + 1);
+            Console.WriteLine($"Dados Atualizados: {Nome}, {SalarioLiqudo()}");
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, {SalarioLiqudo()}"; 
+        }
+    }
+}
---
+++ Ex04/Ex04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionarios funcionario = new Funcionarios();
+            Console.WriteLine("Dados do Funcionario: ");
+            Console.Write("Nome: ");
+            funcionario.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto pago: ");
+            funcionario.Imposto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Dados do funcionário cadastrado: {funcionario}");
+
+            Console.Write($"Aumento percentual do salário: ");
+            double porcentagem = double.Parse(Console.ReadLine());
+            funcionario.AumentarSalario(porcentagem);
+
+        }
+    }
+}
