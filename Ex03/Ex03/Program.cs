a6622c1165c9144209084cf1b9b669a0ee6932db
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-11 13:47:13 -0300

    Adicionado Ex03

---
+++ Ex03/Ex03/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Ex03
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.WriteLine("Insira as dimenções do triângulo: ");
+            Console.Write("Largura - ");
+            retangulo.Largura = double.Parse(Console.ReadLine());
+            Console.Write("Altura - ");
+            retangulo.Altura = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Área: {retangulo.Area().ToString("F2")}cm²");
+            Console.WriteLine($"Perímetro: {retangulo.Perimetro().ToString("F2")}cm");
+            Console.WriteLine($"Diagonal: {retangulo.Diagonal().ToString("F2")}cm");
+        }  
+    }
+}
---
+++ Ex03/Ex03/Retangulo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex03
+{
+    public class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area()
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro()
+        {
+            return 2 * Altura + 2 * Largura;
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt(Math.Pow(Altura, 2)+Math.Pow(Largura, 2));
+        }
+    }
+}
