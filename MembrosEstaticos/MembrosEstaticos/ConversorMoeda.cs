506f35630abd80440852f1a706e150f3c169849d
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-11 15:14:37 -0300

    Adiciona programa de MembrosEstaticos

---
+++ MembrosEstaticos/MembrosEstaticos/ConversorMoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembrosEstaticos
+{
+    public static class ConversorMoeda
+    {
+        public static float CotacaoDolar;
+
+        public static double QuantidadeDeReaisNecessarios(double dolaresDesejados)
+        {
+            return (dolaresDesejados*CotacaoDolar)*1.06;
+        }
+    }
+}
---
+++ MembrosEstaticos/MembrosEstaticos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace MembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dólar? ");
+            ConversorMoeda.CotacaoDolar = float.Parse(Console.ReadLine());
+            Console.WriteLine("Quantos dólares você vai comprar? ");
+            double dolaresDesejados = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Valor a ser pago em reais: R${ConversorMoeda.QuantidadeDeReaisNecessarios(dolaresDesejados).ToString("F2")}");
+        }
+    }
+}
