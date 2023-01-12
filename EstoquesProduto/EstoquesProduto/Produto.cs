8ee8c691a90035d233c78d7a1e4c746c5a8527b5
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-12 14:22:39 -0300

    Adiciona construtor

--- EstoquesProduto/EstoquesProduto/Produto.cs
+++ EstoquesProduto/EstoquesProduto/Produto.cs
@@ -13,6 +13,16 @@ namespace EstoquesProduto
         public double preco;
         public int quantidade;
 
+        /*
+            Obriga o usuário, ao instanciar essa classe, a informar os seus atributos 
+        */
+        public Produto(string nome, double preco, int quantidade)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidade = quantidade;
+        }
+
         public double ValorTotalEmEstoque()
         {
             double valorTotal = quantidade * preco;
@@ -31,7 +41,7 @@ namespace EstoquesProduto
 
         public override string ToString()
         {
-            return $"Nome: {nome} | Quantidade: {quantidade} | Preço: {preco} | Total: R${ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
+            return $"Nome: {nome} | Quantidade: {quantidade} unidades | Preço: R${preco} | Total: R${ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture)}";
         }
 
 
--- EstoquesProduto/EstoquesProduto/Program.cs
+++ EstoquesProduto/EstoquesProduto/Program.cs
@@ -6,25 +6,27 @@ namespace EstoquesProduto
     {
         static void Main(string[] args)
         {
-            Produto produto1 = new Produto();
             Console.WriteLine("Entre os dados do produto -");
             Console.Write("Nome: ");
-            produto1.nome = Console.ReadLine();
+            string nome = Console.ReadLine();
             Console.Write("Preço: ");
-            produto1.preco = double.Parse(Console.ReadLine());
-            Console.WriteLine("Quantidade: ");
-            produto1.quantidade = int.Parse(Console.ReadLine());
+            double preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            
+            Produto produto1 = new Produto(nome, preco, quantidade);
 
+            
             Console.WriteLine($"Dados do Produto - " + produto1);
 
             Console.Write("Digite o número de produtos a ser adicionado: ");
-            int quantidade = int.Parse(Console.ReadLine());
-            produto1.AdicionarProdutos(quantidade);
+            int qtd = int.Parse(Console.ReadLine());
+            produto1.AdicionarProdutos(qtd);
             Console.WriteLine($"Dados do Produto - " + produto1);
 
             Console.Write("Digite o número de produtos a ser removido: ");
-            quantidade = int.Parse(Console.ReadLine());
-            produto1.RemoverProdutos(quantidade);
+            qtd = int.Parse(Console.ReadLine());
+            produto1.RemoverProdutos(qtd);
             Console.WriteLine($"Dados do Produto - " + produto1);
         }
     }
