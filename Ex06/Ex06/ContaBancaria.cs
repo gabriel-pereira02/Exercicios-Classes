48b8a0d88abf70165a843186edfe358500b5bd22
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-13 18:37:47 -0300

    Adiciona Ex06

---
+++ Ex06/Ex06/ContaBancaria.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex06
+{
+    class ContaBancaria
+    {
+        private double SaldoConta;
+        public int NumContaBancaria { get; private set; }
+        public string NomeTitular { get; set; }
+
+        public ContaBancaria(int numContaBancaria , string nomeTitular)
+        {
+            NumContaBancaria = numContaBancaria;
+            NomeTitular = nomeTitular;
+            SaldoConta = 0;
+        }
+
+        public ContaBancaria(int numContaBancaria, string nomeTitular, double saldoConta) : this(numContaBancaria, nomeTitular)
+        {
+            SaldoConta = saldoConta;
+        }
+
+
+        public void Depositar(double valorDepositado)
+        {
+            SaldoConta += valorDepositado;
+            Console.WriteLine($"Conta: {NumContaBancaria}, Nome do Titular: {NomeTitular}, Saldo da Conta: R${SaldoConta}");
+        }
+
+        public void Sacar(double valorSaque)
+        {
+            SaldoConta -= (valorSaque+5);
+            Console.WriteLine($"Conta: {NumContaBancaria}, Nome do Titular: {NomeTitular}, Saldo da Conta: R${SaldoConta}");
+        }
+
+        public override string ToString()
+        {
+            return $"Conta: {NumContaBancaria}, Nome do Titular: {NomeTitular}, Saldo da Conta: R${SaldoConta}";
+        }
+    }
+}
---
+++ Ex06/Ex06/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Ex06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria contaBancaria;
+            Console.Write("Insira nome do titular: ");
+            string nomeTitular = Console.ReadLine();
+            Console.Write("Insira o número da conta: ");
+            int numConta = int.Parse(Console.ReadLine());
+            Console.Write("Haverá deposito inicial? [S/N] ");
+            string opt = Console.ReadLine();
+            if (opt == "S")
+            {
+                Console.Write("Insira o deposito inicial: ");
+                double saldoInicial = double.Parse(Console.ReadLine());
+                contaBancaria = new ContaBancaria(numConta, nomeTitular, saldoInicial);
+            }
+            else
+            {
+                contaBancaria = new ContaBancaria(numConta, nomeTitular);
+            }
+
+            Console.WriteLine($"Dados da Conta: {contaBancaria}");
+
+            Console.Write("Insira a quantidade que será depositada: ");
+            double dinheiro = double.Parse(Console.ReadLine());
+
+            contaBancaria.Depositar(dinheiro);
+
+            Console.Write("Insira a quantidade que será sacada: ");
+            dinheiro = double.Parse(Console.ReadLine());
+            
+            contaBancaria.Sacar(dinheiro);
+        }
+    }
+}
