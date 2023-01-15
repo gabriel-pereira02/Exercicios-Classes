461f059c78e5ff3750cd11f1fd1d3d7cd5db451c
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-15 13:41:25 -0300

    Adiciona NullableProps

---
+++ NullableProps/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace NullableProps
+{
+    class Program{
+        static void Main(string[] args){
+            double? x = null;
+            double? y = 10;
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if(x.HasValue){
+                Console.WriteLine(x.Value);
+            } 
+            else{
+                Console.WriteLine("X é nulo");
+            }
+
+            if(y.HasValue){
+                 Console.WriteLine(y.Value);
+            } 
+            else{
+                Console.WriteLine("Y é nulo");
+            }
+
+            double a = x ?? 10;
+            Console.WriteLine(a);
+            Console.WriteLine("------");
+            x = 20;
+            a = x ?? a;
+            Console.WriteLine(a);
+        }
+    }
+}
