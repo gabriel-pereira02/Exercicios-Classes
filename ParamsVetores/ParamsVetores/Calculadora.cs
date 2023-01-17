5e4c2495c4f66dcb595b42f3ffa083457cc06c55
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-17 16:36:07 -0300

    Adiciona exercício de soma com params

---
+++ ParamsVetores/ParamsVetores/Calculadora.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsVetores
+{
+    public static class Calculadora
+    {
+        public static double Soma(params int[] num)
+        {
+            double soma = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                soma += num[i];
+            }
+            return soma;
+        }
+    }
+}
---
+++ ParamsVetores/ParamsVetores/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ParamsVetores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calculadora.Soma(2, 3, 5));
+        }
+    }
+}
