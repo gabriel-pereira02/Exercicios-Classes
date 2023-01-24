92ce98e9c6babcc98c8631c20788c3638bee0b9f
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-24 14:48:47 -0300

    .

--- Listas/Program.cs
+++ Listas/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Listas
+{
+    class Program{
+        static void Main(string[] args){
+            List<string> lista = new List<string>();
+            lista.Add("Gabriel");
+            lista.Add("João");
+            lista.Add("André");
+            lista.Insert(0, "Ana");
+            foreach(string obj in lista){
+                Console.WriteLine($"{obj}...");
+            }
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"{lista.Find(x => x[0] == 'G')}...");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"{lista.Count}");
+        }
+    }
+}
