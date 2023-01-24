1fc1e1680972d23e02e6c1af65128c467366eca0
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-24 14:09:22 -0300

    Adiciona exercícios com o laço foreach

---
+++ LacoForeach/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LacosForeach
+{
+    class Program{
+        static void Main(string[] args){
+            string[] arr = new string[]{"João", "Maria", "Gabriel"};
+            foreach (string nomes in arr)
+            {
+                Console.Write($"{nomes}...");
+            }
+        }
+    }
+}
