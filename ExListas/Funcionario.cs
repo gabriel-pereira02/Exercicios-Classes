d07cd50f857bbae3a0bca554a0e91b042a1e43a0
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2023-01-25 14:19:29 -0300

    Construtor adicionado

--- ExListas/Funcionario.cs
+++ ExListas/Funcionario.cs
@@ -2,6 +2,19 @@ namespace ExListas
 {
     public class Funcionario
     {
-        
+        public string Nome { get; set; }
+        public int Id { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(string nome, int id, double salario)
+        {
+            Nome = nome;
+            Id = id;
+            Salario = salario;
+        }
+
+        public void AumentarSalario(float porcentagem){
+            Salario = Salario * (1+(porcentagem/100));
+        }
     }
 }
--- ExListas/Program.cs
+++ ExListas/Program.cs
@@ -1,7 +1,20 @@
 ﻿namespace ExListas{
     class Program{
         static void Main(string[] args){
-            
+            List<Funcionario> listaFuncionarios = new List<Funcionario>();
+            Console.Write("Quantos funcionários serão registrados? ");
+            int qtdFuncionarios = int.Parse(Console.ReadLine());
+            for(int i = 0; i < qtdFuncionarios; i++){
+                Console.WriteLine($"Funcionario #{i+1}");
+                Console.WriteLine("Digite o nome do funcionário: ");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Digite o id do funcionário: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o salário do funcionário: ");
+                double salario = double.Parse(Console.ReadLine());
+
+                listaFuncionarios.Add(new Funcionario(nome, id, salario));
+            }
         }
     }
 }
